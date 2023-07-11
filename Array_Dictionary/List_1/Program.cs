0097e8e186bbde7830ef8429c78631638d98279e
HJ <104305435+w8err@users.noreply.github.com>
2023-07-11 11:10:20 +0900

    230711

---
+++ Array_Dictionary/List_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace List_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 배열의 크기를 유동적으로 늘릴 수 있는 동적 배열 = List
+        }
+    }
+}
