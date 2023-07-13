90c738f5c9f8ecdb0bfa4ea171e1fe37ba0168a2
HJ <104305435+w8err@users.noreply.github.com>
2023-07-13 14:25:16 +0900

    230713

---
+++ Array_Dictionary/6_Interface/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp
+{
+    class Program
+    {
+        abstract class Monster          // abstract = 추상 클래스. 여기선 개념적으로만 존재함. 반드시 구현해야 하기 때문에 버그를 찾기 용이
+        {
+            public abstract void Shout();
+        }
+
+        abstract class Flayble
+        {
+            public abstract void Fly();
+
+    }
+}
